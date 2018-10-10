7e9b952eadc651638c9dd364508bc38aa63a06b1
jimbono05 <jimspenceuk@gmail.com>
2018-10-10 11:35:13 +0100

    [JS] Removing server.js

--- Application/src/components/App/App.js
+++ Application/src/components/App/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <PageHeader headerText="Graphic Request Jim"/>
+                <PageHeader headerText="Graphic Request - 11:34am 10/10/2018"/>
                 <Content/>
             </div>
         );
